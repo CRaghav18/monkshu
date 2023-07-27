758758831055e349bd2b656852dc0b5a495ed92c
Raghav <cxragav@gmail.com>
2023-07-27 11:01:33 +0530

    updates

--- training/backend/getUser.js
+++ training/backend/getUser.js
@@ -1,19 +1,36 @@
 
-let getUser = function (db, res) {
-
-    let query = "SELECT * FROM Users";
-
-    db.all(query, function (err, data) {
-
-        if (err) {
-            console.log(err.message);
-        } else {
-
-            res.send(data)
-        }
+let getUser = function (db, req, res) {
 
 
-    })
+    let query1 = "SELECT * FROM Users";
+    let query2 = `SELECT * FROM Users WHERE TYPE = "Member" `
+
+
+    if (req.body.type === 'Librarian') {
+        db.all(query2, function (err, data) {
+
+            if (err) {
+                console.log(err.message);
+            } else {
+
+                res.send(data)
+            }
+
+
+        })
+    } else {
+        db.all(query1, function (err, data) {
+
+            if (err) {
+                console.log(err.message);
+            } else {
+
+                res.send(data)
+            }
+
+
+        })
+    }
 
 }
 
